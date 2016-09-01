c7df7628b9f8ab78a83662b4ada749c50575f24a
Henrique Pin <pin.henrique03@gmail.com>
2016-09-01 15:05:34 -0300

    visitor creates a job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,9 +2,18 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
   def new
     @job = Job.new
   end
+
   def create
+    @job = Job.create job_parameters
+    redirect_to @job
+  end
+
+  private
+  def job_parameters
+    params.require(:job).permit(:title, :location, :category, :company, :description)
   end
 end
