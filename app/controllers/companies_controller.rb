d896f5d6f2d2c92c02c9d9d88c9fe3e1b0b3d418
Henrique Pin <pin.henrique03@gmail.com>
2016-09-05 09:56:01 -0300

    user create companies

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,19 @@
+class CompaniesController < ApplicationController
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.create company_parameters
+    redirect_to @company
+  end
+
+  private
+  def company_parameters
+    params.require(:company).permit(:name, :location, :mail, :phone)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jobs, only: [:show, :new, :create, :edit, :update]
+  resources :companies, only: [:show, :new, :create]
 end
